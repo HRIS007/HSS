6ccfb3ac6418cf0cb87e0f03a8b413960f4aa63c
Himalay Chauhan <himalay.chauhan@hotmail.com>
2020-12-22 20:04:23 +0530

    Enabled background location tracking

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,53 +1,51 @@
 import { Component } from '@angular/core';
-import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationResponse,BackgroundGeolocationEvents } from '@ionic-native/background-geolocation/ngx';
+import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationEvents, BackgroundGeolocationResponse } from '@ionic-native/background-geolocation/ngx';
 import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
-import { NavController } from '@ionic/angular';
 
-declare var window;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  location: any;
+
+  trackedLocations: Array<any> = [];
   config: BackgroundGeolocationConfig = {
     desiredAccuracy: 10,
     stationaryRadius: 20,
     distanceFilter: 30,
     debug: true, //  enable this hear sounds for background-geolocation life-cycle.
     stopOnTerminate: false, // enable this to clear background location settings when the app terminates
+    interval: 5000,
+    notificationTitle: 'Tracking your steps!'
   };
+
   constructor(
-    public navCtrl: NavController,
     private backgroundGeolocation: BackgroundGeolocation,
     private localNotifications: LocalNotifications
   ) {
-   
-    this.location = [];
-  }
-  showNotification(data){
-    // Schedule a single notification
-    this.localNotifications.schedule({
-      id: 1,
-      text: JSON.stringify(data),
-      sound: 'file://sound.mp3',
-      data: { secret: "key" }
+    this.backgroundGeolocation.configure(this.config)
+      .then((location: BackgroundGeolocationResponse) => {
+
+        /* this.showNotification(location); */
+
+        /*  this.trackedLocations.push(location); */
+        // IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
+        // and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
+        // IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
+        /*  this.backgroundGeolocation.finish(); // FOR IOS ONLY */
+
+      });
+
+
+    this.backgroundGeolocation.on(BackgroundGeolocationEvents.location).subscribe((result) => {
+      console.log('location subscription: ', JSON.stringify(result));
+      this.showNotification(result);
+      this.trackedLocations.push(result);
+      console.log('trackedLocations ', this.trackedLocations);
     });
   }
-  StartBackroundTracking() {
-    window.app.backgroundGeolocation.start();
-  }
-  StopBackroundTracking() {
-    window.app.backgroundGeolocation.stop();
-  }
-  GetGeoLocation() {
-   this.location = (JSON.parse(localStorage.getItem('location'))== null)?[]:JSON.parse(localStorage.getItem('location'))
-   console.log("this.location")
-  }
-  Clearlocation() {
-    localStorage.removeItem('location');
-  }
+
   startBackgroundGeolocation() {
     // start recording location
     this.backgroundGeolocation.start();
@@ -58,6 +56,14 @@ export class HomePage {
     this.backgroundGeolocation.stop();
   }
 
-
+  showNotification(data: any) {
+    // Schedule a single notification
+    this.localNotifications.schedule({
+      id: 1,
+      text: JSON.stringify(data),
+      sound: 'file://sound.mp3',
+      data: { secret: 'key' }
+    });
+  }
 
 }
